64f51775b89932eb1162f7ce6227b09b16d3dcdd
Jerry He <jerryhe365@gmail.com>
2024-03-05 19:39:47 -0500

    Update input text color

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -23,7 +23,7 @@ const LoginPage = () => {
                 type="text"
                 id="username"
                 name="username"
-                className="bg-white mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors <duration-300"
+                className="bg-white text-gray-700 mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors <duration-300"
               ></input>
             </div>
             <div>
@@ -37,7 +37,7 @@ const LoginPage = () => {
                 type="text"
                 id="email"
                 name="email"
-                className="bg-white mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors <duration-300"
+                className="bg-white text-gray-700 mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors <duration-300"
               ></input>
             </div>
             <div>
@@ -51,13 +51,13 @@ const LoginPage = () => {
                 type="password"
                 id="password"
                 name="password"
-                className="bg-white mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors <duration-300"
+                className="bg-white text-gray-700 mt-1 p-2 w-full border rounded-md focus:border-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors <duration-300"
               ></input>
             </div>
             <div>
               <button
                 type="submit"
-                className="w-full bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors <duration-300"
+                className="w-full  bg-black text-white p-2 rounded-md hover:bg-gray-800 focus:outline-none focus:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors <duration-300"
               >
                 Sign Up
               </button>
