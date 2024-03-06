a3e5b7277ddebd4ff830b528cfd57d38d8275954
Sathvik-P <sathvikp308@gmail.com>
2024-03-05 19:04:53 -0500

    Basic Code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import LoginPage from "./pages/LoginPage"
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <LoginPage />
     </>
   );
 }
