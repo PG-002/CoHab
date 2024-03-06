4a787235aede40124c432066a66912e34c2d9704
Jerry He <jerryhe365@gmail.com>
2024-03-05 19:12:02 -0500

    Login page code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import LoginPage from "./pages/LoginPage"
+import LoginPage from "./pages/LoginPage";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -9,8 +9,8 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <LoginPage />
+      <h1> YO</h1>
     </>
   );
 }
