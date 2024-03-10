4e63c5de068ccfa98ddf805675cfa4191732eec4
Ethan Fuller <etjamir14@gmail.com>
2024-03-09 22:19:27 -0500

    Update register.dart

--- cohab_mobile/lib/register.dart
+++ cohab_mobile/lib/register.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
 
-class Register extends StatelessWidget{
-  const Register({super.key});
+class Register extends StatelessWidget {
+  const Register({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
-
       body: Column(
         children: [
-            Text('Welcome To Cohab!',style: TextStyle(color: Colors.black)),
-            Text('Your all in one roommate assistant',style: TextStyle(color: Colors.black))
-                  ]
-      )
-    ,
+          Text(
+            'Welcome To Cohab!',
+            style: TextStyle(color: Colors.black),
+          ),
+          Text(
+            'Your all in one roommate assistant',
+            style: TextStyle(color: Colors.black),
+          ),
+          Container(
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                hintText: 'Example@gmail.com'
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
-  
-}
