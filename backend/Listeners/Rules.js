b28adf085d40502c13ae8a3cafa0527c5734b6b8
Evan <pilling2003@gmail.com>
2024-03-18 16:43:37 -0400

    Added grocery and rule socket event functionality

---
+++ backend/Listeners/Rules.js
@@ -0,0 +1,21 @@
+const House = require('../Models/House');
+
+module.exports = (socket, io) => {
+    socket.on('createRule', async rule => {
+        await House.findOneAndUpdate({ _id : socket.room }, { $push : { rules : rule } })
+            .then(rules => io.to(socket.room).emit('rulesChange', { rules : rules }))
+            .catch(err => console.log(err));
+    });
+
+    socket.on('modifyRule', async rule => {
+        await House.findOneAndUpdate({ _id : socket.room, rules : { $elemMatch : { _id : rule._id } } }, { $set : { 'rules' : rule } })
+            .then(rules => io.to(socket.room).emit('rulesChange', { rules : rules }))
+            .catch(err => console.log(err));
+    });
+
+    socket.on('deleteRule', async rule => {
+        await House.findOneAndUpdate({ _id : socket.room }, { $pull : { rules : rule } })
+            .then(rules => io.to(socket.room).emit('rulesChange', { rules : rules }))
+            .catch(err => console.log(err));
+    });
+}
