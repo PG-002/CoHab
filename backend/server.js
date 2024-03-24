b6444272b87a23607294ef6e77dee0897eecf125
AustinVosler <austinpvosler@gmail.com>
2024-03-24 10:53:29 -0400

    changed index location

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,8 @@ app.use((req, res, next) => {
 
 if(env.NODE_ENV === 'production')
 {
-    app.use(express.static(path.join(__dirname, "../frontend")));
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend', 'index.html')));
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html')));
 }
 
 // Routes
