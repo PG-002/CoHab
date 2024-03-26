0ea64c64f06507517bea9da59f46cc68f567bd07
Ethan Fuller <etjamir14@gmail.com>
2024-03-25 21:29:34 -0400

    Register WORKS FINALLY

--- cohab_mobile/lib/register_token.dart
+++ cohab_mobile/lib/register_token.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 var token;
 
 Future<void> signUp(String firstName, String lastName, String email, String password) async {
-  final Uri url = Uri.parse('http://10.127.96.231/api/users/signup');
+  final Uri url = Uri.parse('http://10.127.96.231:5003/api/users/signup');
   final Map<String, String> body = {
     'firstName': firstName,
     'lastName': lastName,
@@ -25,14 +25,14 @@ Future<void> signUp(String firstName, String lastName, String email, String pass
     if (response.statusCode == 201) {
       // Successful signup
       token = json.decode(response.body);
-      print('Token: ${token['token']}');
+      //print('Token: ${token['token']}');
     } else {
       // Signup failed
       token = json.decode(response.body);
-      print('Signup failed: ${token['error']}');
+      //print('Signup failed: ${token['error']}');
     }
   } catch (e) {
     // Exception occurred
-    print('Exception occurred: $e');
+    //print('Exception occurred: $e');
   }
 }
