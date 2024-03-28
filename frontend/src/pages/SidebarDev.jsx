62554022f645f68f46266683378345f133ee2feb
Jerry He <jerryhe365@gmail.com>
2024-03-27 21:26:49 -0400

    removed extraneous files

--- frontend/src/pages/SidebarDev.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Sidebar, { SidebarItem } from "./global/Sidebar";
-import { LayoutDashboard } from "lucide-react";
-
-function SidebarDev() {
-  return <div>sidebar dev</div>;
-}
-
-export default SidebarDev;
