8cc49b29bd5c510b5edc767412e6529e7875cf32
Ethan Fuller <etjamir14@gmail.com>
2024-03-27 22:12:08 -0400

    Update web_socket.dart

--- cohab_mobile/lib/web_socket.dart
+++ cohab_mobile/lib/web_socket.dart
@@ -1,47 +1,16 @@
 import 'dart:convert';
-
-
 import 'package:flutter/material.dart';
-import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:web_socket_channel/status.dart' as status;
 
+main() async {
+  final wsUrl = Uri.parse('https://cohab-4fcf8ee594c1.herokuapp.com');
+  final channel = WebSocketChannel.connect(wsUrl);
 
-class WebSocketManager {
-  final WebSocketChannel _channel;
+  await channel.ready;
 
-  WebSocketManager(String serverAddress) : _channel = IOWebSocketChannel.connect(serverAddress);
-
-  Stream<dynamic> get stream => _channel.stream;
-
-  void sendAction(String action, dynamic data) {
-    _channel.sink.add(jsonEncode({'action': action, 'data': data}));
-  }
-
-  void dispose() {
-    _channel.sink.close();
-  }
-}
-
-void main() {
-  final WebSocketManager manager = WebSocketManager('ws://your_server_address');
-
-  // Listening for messages
-  manager.stream.listen((message) {
-    // Parse the message
-    Map<String, dynamic> data = jsonDecode(message);
-
-    // Handle different actions
-    switch (data['action']) {
-      case 'api1':
-      // Handle API 1
-        break;
-      case 'api2':
-      // Handle API 2
-        break;
-    // Add cases for other APIs as needed
-    }
+  channel.stream.listen((message) {
+    channel.sink.add('received!');
+    channel.sink.close(status.goingAway);
   });
-
-  // Sending messages
-  manager.sendAction('api1', {'param1': 'value1'});
 }
