2fc3f3c1db2a1a99fdf18f4512f0453f6575eee6
Ethan Fuller <etjamir14@gmail.com>
2024-03-30 19:03:14 -0400

    Update task_list.dart

--- cohab_mobile/lib/task_list.dart
+++ cohab_mobile/lib/task_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lucide_icons/lucide_icons.dart';
 import 'web_socket.dart';
 
 
@@ -22,8 +23,6 @@ class TaskListPage extends StatelessWidget {
 }
 
 class TaskList extends StatefulWidget {
-
-
   const TaskList({super.key});
 
   @override
@@ -31,16 +30,51 @@ class TaskList extends StatefulWidget {
 }
 
 class _TaskListState extends State<TaskList> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Task List'),
+        title: const Text(
+          '                     Task List',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color(0xFF14532d),
+        iconTheme: const IconThemeData(color: Colors.white),
+        titleSpacing: 0, // Adjust the spacing
       ),
-
+      body: const Column(
+        children: [
+          SizedBox(height: 20),
+          AddTasksButton(),
+        ],
+      ),
     );
   }
-
 }
 
+
+
+class AddTasksButton extends StatelessWidget {
+  const AddTasksButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipOval(
+      child: Material(
+        color: const Color(0xFF14532d),
+        child: InkWell(
+          onTap: () {},
+          child: const SizedBox(
+            width: 50, // Increase width to make the icon bigger
+            height: 50, // Increase height to make the icon bigger
+            child: Icon(
+              LucideIcons.plusCircle,
+              color: Colors.white,
+              size: 36, // Adjust the size of the icon
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
