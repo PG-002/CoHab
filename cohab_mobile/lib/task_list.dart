493466b7b58609985da3ae999eb6fafc1d919926
Ethan Fuller <etjamir14@gmail.com>
2024-03-30 17:48:12 -0400

    update

--- cohab_mobile/lib/task_list.dart
+++ cohab_mobile/lib/task_list.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:web_socket_channel/io.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
+import 'web_socket.dart';
+
 
 class Task {
   final String id;
@@ -15,106 +15,32 @@ class TaskListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: TaskList(channel: IOWebSocketChannel.connect('wss://cohab-4fcf8ee594c1.herokuapp.com:5003')),
+    return const Scaffold(
+      body: TaskList(),
     );
   }
 }
 
 class TaskList extends StatefulWidget {
-  final WebSocketChannel channel;
 
-  const TaskList({super.key, required this.channel});
+
+  const TaskList({super.key});
 
   @override
   createState() => _TaskListState();
 }
 
 class _TaskListState extends State<TaskList> {
-  List<Task> tasks = [];
-
-  @override
-  void initState() {
-    super.initState();
-    // Listen for incoming messages from the WebSocket server
-    widget.channel.stream.listen((message) {
-      setState(() {
-        tasks = (message as List).map((task) => Task(task['id'], task['task'], task['completed'])).toList();
-      });
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Task List',
-          style: TextStyle(color: Colors.white),
-        ),
-        centerTitle: true,
-        elevation: 1,
-        backgroundColor: Colors.black,
+        title: const Text('Task List'),
       ),
-      body: Column( // Wrap with a Column to place multiple widgets vertically
-        children: [
-          MaterialButton( // Place the MaterialButton within the Column
-            onPressed: () {
-              // Handle button onPressed event
-            },
-            color: const Color(0xFF14532d),
-            child: const Text(
-              'Button Text',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-          ),
-          Expanded( // Expanded to ensure the ListView takes up all available space
-            child: ListView.builder(
-              itemCount: tasks.length,
-              itemBuilder: (context, index) {
-                final task = tasks[index];
-                return ListTile(
-                  title: Text(task.task),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.edit),
-                        onPressed: () {
-                          // Implement modify task functionality
-                        },
-                      ),
-                      IconButton(
-                        icon: Icon(task.completed ? Icons.check_box : Icons.check_box_outline_blank),
-                        onPressed: () {
-                          // Send a message to toggle the completion status of the task
-                          widget.channel.sink.add('{"id": "${task.id}", "task": "${task.task}", "completed": ${!task.completed}}');
-                        },
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed: () {
-                          // Send a message to delete the task
-                          widget.channel.sink.add('{"id": "${task.id}", "delete": true}');
-                        },
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
+
     );
   }
 
-  @override
-  void dispose() {
-    widget.channel.sink.close();
-    super.dispose();
-  }
 }
 
