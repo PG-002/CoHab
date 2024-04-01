31f4d83e69416a88ddd0515796178402b3281ca4
ellieteter <156714215+ellieteter@users.noreply.github.com>
2024-04-01 15:38:24 -0400

    email verify works with login!

--- cohab_mobile/lib/email_verify.dart
+++ cohab_mobile/lib/email_verify.dart
@@ -1,6 +1,6 @@
+import 'package:cohab_mobile/houseoptions.dart';
 import 'package:cohab_mobile/register.dart';
 import 'package:flutter/material.dart';
-import 'package:cohab_mobile/task_list.dart';
 import 'token.dart';
 
 class VerificationPage extends StatefulWidget {
@@ -15,7 +15,8 @@ class _VerificationPageState extends State<VerificationPage> {
   void initState() {
     super.initState();
     // Call sendVerification when the page is initialized
-    //sendVerification(userId); // Assuming userId is available
+    print(userId);
+    sendVerification(userId); // Assuming userId is available
   }
 
   String _verificationCode = '';
@@ -26,46 +27,46 @@ class _VerificationPageState extends State<VerificationPage> {
     });
   }
 
-  // void _handleEmail(BuildContext context) async {
-  //   print(_verificationCode);
-  //   try {
-  //     //await verifyUsers(userId, _verificationCode);
-  //
-  //     if (_verificationCode.isNotEmpty) {
-  //       // Call a function to verify the code
-  //       // For example, you can send a request to your backend to verify the code
-  //       if (userIsVerified(decodedToken)) {
-  //         if (!context.mounted) return;
-  //         Navigator.pushReplacement(
-  //           context,
-  //           MaterialPageRoute(builder: (context) => const TaskListPage()),
-  //         );
-  //       } else {
-  //         if (!context.mounted) return;
-  //         ScaffoldMessenger.of(context).showSnackBar(
-  //           const SnackBar(
-  //             content: Text('Verification code is not valid.'),
-  //           ),
-  //         );
-  //       }
-  //     } else {
-  //       // Show an error message indicating that the verification code is empty
-  //       if (!context.mounted) return;
-  //       ScaffoldMessenger.of(context).showSnackBar(
-  //         const SnackBar(
-  //           content: Text('Please enter a verification code.'),
-  //         ),
-  //       );
-  //     }
-  //   } catch (e) {
-  //     if (!context.mounted) return;
-  //     ScaffoldMessenger.of(context).showSnackBar(
-  //       const SnackBar(
-  //         content: Text('An error occurred. Please try again later.'),
-  //       ),
-  //     );
-  //   }
-  // }
+  void _handleEmail(BuildContext context) async {
+    //print(_verificationCode);
+    try {
+      await verifyUsers(userId, _verificationCode);
+      
+      if (_verificationCode.isNotEmpty) {
+        // Call a function to verify the code
+        // For example, you can send a request to your backend to verify the code
+        if (isVerified == true) {
+          if (!context.mounted) return;
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const HouseOptions()),
+          );
+        } else {
+          if (!context.mounted) return;
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Verification code is not valid.'),
+            ),
+          );
+        }
+      } else {
+        // Show an error message indicating that the verification code is empty
+        if (!context.mounted) return;
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Please enter a verification code.'),
+          ),
+        );
+      }
+    } catch (e) {
+      if (!context.mounted) return;
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('An error occurred. Please try again later.'),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -76,10 +77,7 @@ class _VerificationPageState extends State<VerificationPage> {
       body: Center(
         child: Container(
           padding: const EdgeInsets.all(20.0),
-          width: MediaQuery
-              .of(context)
-              .size
-              .width * 0.8,
+          width: MediaQuery.of(context).size.width * 0.8,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -100,19 +98,19 @@ class _VerificationPageState extends State<VerificationPage> {
                 ),
               ),
               const SizedBox(height: 20.0),
-              // ElevatedButton(
-              //   onPressed: () => _handleEmail(context), // Pass context here
-              //   style: ElevatedButton.styleFrom(
-              //     backgroundColor: Colors.blue,
-              //   ),
-              //   child: const Text(
-              //     'Submit',
-              //     style: TextStyle(
-              //       color: Colors.white,
-              //       fontSize: 16,
-              //     ),
-              //   ),
-              // ),
+              ElevatedButton(
+                onPressed: () => _handleEmail(context), // Pass context here
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                ),
+                child: const Text(
+                  'Submit',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
             ],
           ),
         ),
