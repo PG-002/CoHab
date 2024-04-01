e537db5c2a1a38f58ae879548250f837c3663588
nanihanny <102572443+nanihanny@users.noreply.github.com>
2024-04-01 01:27:57 -0400

    updates

--- cohab_mobile/lib/join_house_api.dart
+++ cohab_mobile/lib/join_house_api.dart
@@ -9,8 +9,8 @@ var decodedToken;
 
 bool check = false;
 
-Future<void> joinHouse(String joinHouseCode, BuildContext context) async {
-  final Uri url = Uri.parse('https://cohab-4fcf8ee594c1.herokuapp.com/');
+Future<bool> joinHouse(String joinHouseCode) async {
+  final Uri url = Uri.parse('https://cohab-4fcf8ee594c1.herokuapp.com/api/house/join');
   final Map<String, dynamic> body = {
     'joinHouseCode': joinHouseCode,
   };
@@ -25,16 +25,21 @@ Future<void> joinHouse(String joinHouseCode, BuildContext context) async {
     );
 
     if (response.statusCode == 200) {
-      // Joined house successfully, next, go to homepage
-      final jsonResponse = json.decode(response.body);
-      token = jsonResponse['token']; // Extracting the token string
-      decodedToken = JwtDecoder.decode(token);
-      userId = decodedToken['user']['_id'];
-
+      // Joined house successfully
+      // final jsonResponse = json.decode(response.body);
+      // token = jsonResponse['token']; // Extracting the token string
+      // decodedToken = JwtDecoder.decode(token);
+      // userId = decodedToken['user']['_id'];
+      check = true;
+      return true;
     } else {
       // Join failed
+      check = false;
+      return false;
     }
   } catch (e) {
     // Exception occurred
+    check = false;
+    return false;
   }
 }
