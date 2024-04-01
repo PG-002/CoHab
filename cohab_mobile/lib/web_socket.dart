53a3024f022d43ceb28a8d9610b3f4549fb868e8
Ethan Fuller <etjamir14@gmail.com>
2024-04-01 00:07:20 -0400

    Update web_socket.dart

--- cohab_mobile/lib/web_socket.dart
+++ cohab_mobile/lib/web_socket.dart
@@ -7,12 +7,13 @@ Future<void> initSocket() async {
   socket = io.io(
     'https://cohab-4fcf8ee594c1.herokuapp.com/',
     <String, dynamic>{
-      'reconnectionDelayMax': 10000,
-      'auth': {'token': decodedToken.toString()}
+      'transports': ['websocket'], // Use 'websocket' instead of 'websockets'
+      'auth': {'token': decodedToken.toString()},
     },
   );
 
   // Connect to the WebSocket server
   socket.connect();
 
+  print(socket.connected);
 }
