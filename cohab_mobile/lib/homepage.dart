b17587bff6f25891a5d6365444e49bf5aa9978a9
Ethan Fuller <etjamir14@gmail.com>
2024-04-02 12:52:14 -0400

    Update homepage.dart

--- cohab_mobile/lib/homepage.dart
+++ cohab_mobile/lib/homepage.dart
@@ -21,6 +21,96 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 120,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const CalendarHomePage()),
+                      );
+                    },
+                    child: Ink(
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF14532d), // Fill the button with green color
+                        borderRadius: BorderRadius.circular(8.0),
+                        border: Border.all(color: const Color(0xFF14532d)), // Green border
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => const CalendarHomePage()),
+                              );
+                            },
+                            child: const Icon(
+                              LucideIcons.layoutDashboard,
+                              color: Colors.white,
+                              size: 48,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          const Text(
+                            'Dashboard',
+                            style: TextStyle(color: Colors.white, fontSize: 17),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 40),
+                SizedBox(
+                  width: 120,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const CalendarHomePage()),
+                      );
+                    },
+                    child: Ink(
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF14532d), // Fill the button with green color
+                        borderRadius: BorderRadius.circular(8.0),
+                        border: Border.all(color: const Color(0xFF14532d)), // Green border
+                      ),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => const CalendarHomePage()),
+                              );
+                            },
+                            child: const Icon(
+                              LucideIcons.locateFixed,
+                              color: Colors.white,
+                              size: 48,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          const Text(
+                            'Locator',
+                            style: TextStyle(color: Colors.white, fontSize: 17),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 80),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
