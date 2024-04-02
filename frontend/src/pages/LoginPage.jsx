1d8873c3780648308f67b60ca577c6573236ff79
Jerry He <jerryhe365@gmail.com>
2024-04-02 03:48:16 -0400

    Revert "Update LoginPage.jsx"
    This reverts commit 00563fb92298ccb6612f066386183e34358219f0.

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -23,7 +23,7 @@ const LoginPage = () => {
         password: password,
       });
 
-      const response = await fetch("https://cohab-4fcf8ee594c1.herokuapp.com/api/users/login", {
+      const response = await fetch("http://cohab-4fcf8ee594c1.herokuapp.com/api/users/login", {
         // Adjust URL as necessary
         method: "POST",
         headers: {
