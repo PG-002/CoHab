6c32ffa586a28f683505756a628712a427e531b7
Jerry He <jerryhe365@gmail.com>
2024-04-02 23:59:23 -0400

    Fixes to application Socket IO

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,76 +1,80 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
-import '../components/Chat.css';
+import "../components/Chat.css";
 import Messages from "../components/Messages";
 import SendMessageForm from "../components/SendMessageForm";
 
-function Chat({socket}) {
+function Chat({ socket }) {
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    fetch("https://cohab-4fcf8ee594c1.herokuapp.com/api/users/getHouse", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userId: decodedToken.userId }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.house) {
+          setMessages(data.house.groupChat);
+        } else {
+          // Handle the case where the house is not found
+          console.error("House not found:", data.error);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching house:", error);
+        navigate("/login"); // Redirect on error or handle differently
+      });
+    console.log("I am being mounted");
+  }, [navigate]);
 
-    const navigate = useNavigate();
+  const decodedToken = jwtDecode(localStorage.getItem("sessionId"));
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
-
-    useEffect(() => {
-      fetch('https://cohab-4fcf8ee594c1.herokuapp.com/api/users/getHouse', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ userId: decodedToken.userId }),
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+  useEffect(() => {
+    if (socket) {
+      socket.on("groupChatChange", (data) => {
+        // console.log(data.messages);
+        setMessages(data.messages);
+      });
     }
-    return response.json();
-  })
-  .then(data => {
-    if (data.house) {
-      setMessages(data.house.groupChat);
-    } else {
-      // Handle the case where the house is not found
-      console.error('House not found:', data.error);
-    }
-  })
-  .catch(error => {
-    console.error('Error fetching house:', error);
-    navigate('/login'); // Redirect on error or handle differently
-  });
-  console.log("I am being mounted");
-}, [navigate]);
-
-    const decodedToken = jwtDecode(localStorage.getItem('sessionId'));
-    const [currentMessage, setCurrentMessage] = useState('');
-    const [messages, setMessages] = useState([]);
-
-
-    socket.on('groupChatChange', (data) => {
-      // console.log(data.messages);
-      setMessages(data.messages); 
-  });
+  }, [socket]);
 
   const handleSendMessage = (messageContent) => {
-    const messageDate = new Date(); 
+    const messageDate = new Date();
     // console.log(messageContent);
-    socket.emit('sendMessage', messageContent);
-        // console.log("sentBy is " + decodedToken.firstName);
-        // console.log("email is " +  decodedToken.email);
-    setCurrentMessage(''); 
+    socket.emit("sendMessage", messageContent);
+    // console.log("sentBy is " + decodedToken.firstName);
+    // console.log("email is " +  decodedToken.email);
+    setCurrentMessage("");
   };
 
   const handleDeleteMessage = (message) => {
-    socket.emit('deleteMessage', message);
+    socket.emit("deleteMessage", message);
   };
 
   return (
     <div className="message-list-container">
       <h2>Group Chat</h2>
       <div>
-      <Messages messages={messages} userID={decodedToken.email} onDelete={handleDeleteMessage} />
+        <Messages
+          messages={messages}
+          userID={decodedToken.email}
+          onDelete={handleDeleteMessage}
+        />
       </div>
-      <div className='chat-container'>
-      <SendMessageForm sendMessage={handleSendMessage} />
+      <div className="chat-container">
+        <SendMessageForm sendMessage={handleSendMessage} />
       </div>
     </div>
   );
--- frontend/src/pages/TodoList.jsx
+++ frontend/src/pages/TodoList.jsx
@@ -4,141 +4,165 @@ import { jwtDecode } from "jwt-decode";
 import { Link, useNavigate } from "react-router-dom";
 
 function TodoList({ socket }) {
-
   const navigate = useNavigate();
 
   const LogOut = () => {
-      localStorage.removeItem("sessionId");
-      navigate("/");
+    localStorage.removeItem("sessionId");
+    navigate("/");
   };
 
   useEffect(() => {
     // Fetch tasks on mount
     const fetchTasks = async () => {
-      const sessionId = localStorage.getItem('sessionId');
+      const sessionId = localStorage.getItem("sessionId");
       if (!sessionId) {
-        navigate('/login'); // Redirect to login if no session
+        navigate("/login"); // Redirect to login if no session
         return;
       }
-  
+
       const decodedToken = jwtDecode(sessionId);
       const userId = decodedToken.userId;
-  
+
       try {
-        const response = await fetch('https://cohab-4fcf8ee594c1.herokuapp.com/api/users/getHouse', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ userId }),
-        });
-  
+        const response = await fetch(
+          "https://cohab-4fcf8ee594c1.herokuapp.com/api/users/getHouse",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ userId }),
+          }
+        );
+
         if (!response.ok) {
-          throw new Error('Failed to fetch tasks');
+          throw new Error("Failed to fetch tasks");
         }
-  
+
         const data = await response.json();
         if (data.house) {
           setTasks(data.house.tasks); // or however the tasks are stored in your "house" object
         } else {
-          console.error('House not found:', data.error);
+          console.error("House not found:", data.error);
           // Possibly handle a redirect or error state
         }
       } catch (error) {
-        console.error('Error fetching tasks:', error);
-        navigate('/login'); // Redirect or handle error
+        console.error("Error fetching tasks:", error);
+        navigate("/login"); // Redirect or handle error
       }
     };
-  
+
     fetchTasks();
   }, [navigate]);
 
   const [todo, setTodo] = useState("");
-  const [tasks, setTasks] = useState([]); 
+  const [tasks, setTasks] = useState([]);
   const [isEditing, setIsEditing] = useState(null);
   const [editText, setEditText] = useState("");
 
-      socket.on('tasksChange', (data) => {
-          setTasks(data.tasks);
+  useEffect(() => {
+    if (socket) {
+      socket.on("tasksChange", (data) => {
+        setTasks(data.tasks);
       });
+    }
+  }, [socket]);
 
   const handleAddTask = (e) => {
-      e.preventDefault();
-      console.log("I am being called");
-      // Emit the new task to the server
-      socket.emit("createTask", {
-          task: todo,
-          completed: false,
-      });          
-      setTodo("");
+    e.preventDefault();
+    console.log("I am being called");
+    // Emit the new task to the server
+    socket.emit("createTask", {
+      task: todo,
+      completed: false,
+    });
+    setTodo("");
   };
 
   const handleDeleteTask = (task) => {
-      // Emit the delete task event to the server
-      socket.emit("deleteTask", { id : task._id });
-      setTasks(currentTasks => currentTasks.filter(task => task.id !== task._id));
+    // Emit the delete task event to the server
+    socket.emit("deleteTask", { id: task._id });
+    setTasks((currentTasks) =>
+      currentTasks.filter((task) => task.id !== task._id)
+    );
   };
 
-const startEditing = (task) => {
-  setIsEditing(task._id);
-  setEditText(task.task);
-};
+  const startEditing = (task) => {
+    setIsEditing(task._id);
+    setEditText(task.task);
+  };
 
-const handleEditChange = (e) => {
-  setEditText(e.target.value);
-};
+  const handleEditChange = (e) => {
+    setEditText(e.target.value);
+  };
 
-const submitEdit = (e, taskId) => {
-  e.preventDefault();
-  // Emit the updated task to the server
-  socket.emit("modifyTask", {
-    _id: taskId,
-    task: editText,
-    completed: false, // or the current status of the task
-  });
-  setIsEditing(null);
-  setEditText("");
-};
+  const submitEdit = (e, taskId) => {
+    e.preventDefault();
+    // Emit the updated task to the server
+    socket.emit("modifyTask", {
+      _id: taskId,
+      task: editText,
+      completed: false, // or the current status of the task
+    });
+    setIsEditing(null);
+    setEditText("");
+  };
 
   return (
-      <div>
-          <Nav />
-          <form className="form" onSubmit={handleAddTask}>
-              <input
-                  autoFocus
-                  value={todo}
-                  onChange={(e) => setTodo(e.target.value)}
-                  className="input"
-                  placeholder="Enter new task"
-                  required
-              />
-              <button className="form__cta">Add Task</button>
-          </form>
-          <div className="todo__container">
-          {tasks.map((taskItem) => (
-        <div key={taskItem._id} className="todo__item">
-          {isEditing === taskItem._id ? (
-            <form onSubmit={(e) => submitEdit(e, taskItem._id)}>
-              <input autoFocus value={editText} onChange={handleEditChange} />
-              <button type="submit">Save</button>
-              <button onClick={() => setIsEditing(null)}>Cancel</button>
-            </form>
-          ) : (
-            <>
-              <p>{taskItem.task}</p>
-              <div>
-                <button className="commentsBtn" onClick={() => startEditing(taskItem)}>Edit</button>
-                <button className="deleteBtn" onClick={() => handleDeleteTask(taskItem)}>Delete</button>
-              </div>
-            </>
-          )}
-        </div>
-      ))}
+    <div>
+      <Nav />
+      <form className="form" onSubmit={handleAddTask}>
+        <input
+          autoFocus
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          className="input"
+          placeholder="Enter new task"
+          required
+        />
+        <button className="form__cta">Add Task</button>
+      </form>
+      <div className="todo__container">
+        {tasks.map((taskItem) => (
+          <div key={taskItem._id} className="todo__item">
+            {isEditing === taskItem._id ? (
+              <form onSubmit={(e) => submitEdit(e, taskItem._id)}>
+                <input autoFocus value={editText} onChange={handleEditChange} />
+                <button type="submit">Save</button>
+                <button onClick={() => setIsEditing(null)}>Cancel</button>
+              </form>
+            ) : (
+              <>
+                <p>{taskItem.task}</p>
+                <div>
+                  <button
+                    className="commentsBtn"
+                    onClick={() => startEditing(taskItem)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="deleteBtn"
+                    onClick={() => handleDeleteTask(taskItem)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </>
+            )}
           </div>
-          <button onClick={LogOut}>Log Out</button>
-          <button><Link to="/dashboard" className="text-white dark:text-white hover:underline">Dashboard</Link></button>
+        ))}
       </div>
-      
+      <button onClick={LogOut}>Log Out</button>
+      <button>
+        <Link
+          to="/dashboard"
+          className="text-white dark:text-white hover:underline"
+        >
+          Dashboard
+        </Link>
+      </button>
+    </div>
   );
 }
 export default TodoList;
