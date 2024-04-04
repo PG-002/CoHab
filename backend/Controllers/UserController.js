8c5136e4a8da22bcad9a7da697e35d5afafcc25b
RyanGarfinkel <113050972+RyanGarfinkel@users.noreply.github.com>
2024-04-04 17:39:37 -0400

    PassowrdUpdate update, and verifyCode

--- backend/Controllers/UserController.js
+++ backend/Controllers/UserController.js
@@ -1,7 +1,7 @@
 const User = require('../Models/User');
 const House = require('../Models/House');
 const { hash, compare } = require('../Middleware/PasswordHash');
-const { sendCode, verifyCode, deleteCode } = require('../Middleware/Email');
+const { sendCode, getEntry, removeEntry } = require('../Middleware/Email');
 const { createToken, verifyToken, decodeToken } = require('../Middleware/Token');
 
 const retUserObj = user => ({
@@ -96,24 +96,23 @@ const updateUser = async (req, res) => {
 };
 
 const updatePassword = async (req, res) => {
-  const { userId, password } = req.body;
+  const { email, password } = req.body;
+
+  res.status(200);
 
   const hashedPassword = await hash(password);
 
   if (hashedPassword.error)
   {
-    res.status(404);
-    res.json({ error: hashedPassword.error });
+    res.json({ error : hashedPassword.error });
   }
   else
   {
-    await User.findOneAndUpdate({ _id : userId }, { password : hashedPassword.password })
+    await User.findOneAndUpdate({ email : email }, { password : hashedPassword.password })
       .then(() => {
-        res.status(200);
         res.json({ changed : true, error : '' });
       })
-      .catch((e) => {
-        res.status(404);
+      .catch(() => {
         res.json({ changed : false, error : 'The password could not be updated.' });
       });
   }
@@ -144,39 +143,103 @@ const deleteUser = async (req, res) => {
 };
 
 const sendVerification = async (req, res) => {
-  const { id } = req.body;
-  const user = await User.findOne({ _id: id })
+  const { email } = req.body;
+  const user = await User.findOne({ email : email })
     .catch(() => null);
 
   res.status(200);
+
   if(!user)
-    res.json({ sent: false, error: 'User not found.' });
+    res.json({ sent: false, error: 'User with that email could not be found.' });
   else
-    res.json(await sendCode(user));
+    res.json(await sendCode(user, user.verified ? 'password change' : 'account'));
 };
 
-const verifyUser = async (req, res) => {
-  const { id, code } = req.body;
-  const user = await User.findOne({ _id : id })
+const verifyCode = async (req, res) => {
+
+  const { email, code } = req.body;
+  
+  res.status(200);
+
+  const user = await User.findOne({ email : email })
     .catch(() => null);
 
-  res.status(200);
   if(!user)
-    res.json({ error: 'User could not be found.' });
-  else
   {
-    const verificationError = await verifyCode(user, code);
-    if (verificationError)
-      res.json({ verified: false, error: verificationError });
-    else
-      await deleteCode(user, code)
-        .then(async (deleteRespose) =>
-          await User.updateOne({ _id : user._id }, { verified : true, error : '' })
-            .then(() => res.json(deleteRespose))
-            .catch(() => res.json({ verified : false, error : 'Could not update user status.' })))
-        .catch(() => res.json({ verified : false, error : 'VerificationEntry could not be deleted.' }));
+    res.json({ verified : false, token : null, error : 'User with that email could not be found.'});
+    return;
   }
-};
+
+  const entries = await getEntry(user._id);
+
+  if(!entries || entries.length === 0)
+  {
+    res.json({ verfified : false, token : null, error : 'User does not have any pending verification codes.' })
+    return;
+  }
+
+  const entry = entries.find(e => e.code === code);
+
+  if(!entry)
+  {
+    res.json({ verfified : false, token : null, error : 'The code is invalid.' })
+    return;
+  }
+
+  await removeEntry(entry._id);
+
+  if(entry.expDate < Date.now())
+  {
+    res.json({ verified : false, token : null, error : 'This code has expired.' });
+    return;
+  }
+
+  if(entry.type === 'account')
+  {
+    const updated = await User.findByIdAndUpdate(user._id, { verified : true }, { new : true })
+      .catch(() => null);
+
+    if(!updated)
+    {
+      res.json({ verfified : false, token : null, error : 'Error updating the user.' });
+      return;
+    }
+  }
+
+  if(entry.type === 'account' || entry.type === 'password change')
+  {
+    res.json({ verified : true, token : null, error : '' });
+    return;
+  }
+
+  if(entry.type !== 'invite')
+  {
+    res.json({ verified : false, token : null, error : 'Invalid VerfificationEntry type.' });
+    return;
+  }
+
+  const house = await House.findById(entry.houseId)
+    .catch(() => null);
+
+  if(!house)
+  {
+    res.json({ verified : false, token : null, error : 'Invalid houseId.' });
+    return;
+  }
+
+  const houseUpdated = await House.findByIdAndUpdate(house._id, { $push : { members : user.firstName + ' ' + user.lastName } }, { new : true })
+    .catch(() => null);
+
+  if(!houseUpdated)
+  {
+    res.json({ verified : false, token : null, error : 'Error updating the house.' });
+    return;
+  }
+
+  await User.findByIdAndUpdate(user._id, { houseId : house._id })
+    .then(() => res.json({ verified : true, token : createToken({ house : house }), error : '' }))
+    .catch(() => res.json({ verfified : false, token : null, error : 'Error updating the user.' }));
+}
 
 const encode = async (req, res) => {
   res.status(200);
@@ -199,4 +262,4 @@ const decode = async (req, res) => {
   }
 };
 
-module.exports = { signup, login, getHouse, updateUser, updatePassword, deleteUser, sendVerification, verifyUser, encode, decode };
+module.exports = { signup, login, getHouse, updateUser, updatePassword, deleteUser, sendVerification, verifyCode, encode, decode };
