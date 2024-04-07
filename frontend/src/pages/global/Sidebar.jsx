3adad548a24a42c96941e375c288cee83976d5b1
Jerry He <jerryhe365@gmail.com>
2024-04-07 16:30:48 -0400

    Light/Dark for Calendar

--- frontend/src/pages/global/Sidebar.jsx
+++ frontend/src/pages/global/Sidebar.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 const SidebarContext = createContext();
 
 function Sidebar({ userInfo, children }) {
-  const [expanded, setExpanded] = useState(true);
+  const [expanded, setExpanded] = useState(false);
 
   return (
     <aside className="h-screen">
