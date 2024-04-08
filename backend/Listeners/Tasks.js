6ce63432f61d3422d627890322f75d879fccf76a
Evan <pilling2003@gmail.com>
2024-04-08 10:36:53 -0400

    Testing changes

--- backend/Listeners/Tasks.js
+++ backend/Listeners/Tasks.js
@@ -22,7 +22,13 @@ module.exports = (socket, io) => {
     });
 
     socket.on('modifyTask', async task => {
-        await House.findOneAndUpdate({ _id : socket.room, tasks : { $elemMatch : { _id : task._id } } }, { $set : updateObj(task) }, { new : true })
+        await House.find({ _id : socket.room, tasks : { $elemMatch : { _id : task._id } } }, { $set : updateObj(task) }, { new : true })
+            .then(house => io.to(socket.room).emit('tasksChange', { tasks : house.tasks }))
+            .catch(err => console.log(err));
+    });
+
+    socket.on('searchTask', async input => {
+        await House.find({ _id : socket.room, tasks : { task : input.task} } )
             .then(house => io.to(socket.room).emit('tasksChange', { tasks : house.tasks }))
             .catch(err => console.log(err));
     });
@@ -33,3 +39,5 @@ module.exports = (socket, io) => {
             .catch(err => console.log(err));
     });
 }
+
+await MyModel.find({ name: 'john', age: { $gte: 18 } }).exec();
