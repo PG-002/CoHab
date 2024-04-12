595f2b563faa75c125d2ce33501bc84b3e5efb4a
ellieteter <156714215+ellieteter@users.noreply.github.com>
2024-04-12 13:00:54 -0400

    updating to database now!

--- cohab_mobile/lib/location.dart
+++ cohab_mobile/lib/location.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/material.dart';
+/*import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'web_socket.dart'; 
+import 'package:socket_io_client/socket_io_client.dart';
+import 'web_socket.dart';
 
 class LocationTrackerPage extends StatefulWidget {
   const LocationTrackerPage({super.key});
@@ -17,7 +18,13 @@ class _LocationTrackerPageState extends State<LocationTrackerPage> {
   @override
   void initState() {
     super.initState();
+    init();
     _requestLocationPermission();
+  }
+
+
+  void _onMapCreated(GoogleMapController controller) {
+    _mapController = controller;
   }
 
   void _requestLocationPermission() async {
@@ -49,21 +56,51 @@ class _LocationTrackerPageState extends State<LocationTrackerPage> {
         ),
       );
 
+      //print(_currentPosition.latitude);
+      //print(_currentPosition.longitude);
+
+      if (socket.connected) {
+        print('Socket is connected');
+        // Emit location data to server
+        try {
+          socket.emit('updateLocation', {
+            'lat': _currentPosition.latitude,
+            'long': _currentPosition.longitude,
+          });
+
+          socket.on('locationChange', (data) {
+            print("Location update failed: $data");
+          });
+          print("Yay!");
+        } catch (e) {
+          print('Error emitting location data: $e');
+        }
+      } else {
+        print('Socket is not connected');
+      }
+
+      /*if (socket.connected) {
+        print('Socket is connected');
+      } else {
+        print('Socket is not connected');
+      }
       // Emit location data to server
       socket.emit('updateLocation', {
-        'latitude': _currentPosition.latitude,
-        'longitude': _currentPosition.longitude,
+        'lat': _currentPosition.latitude,
+        'long': _currentPosition.longitude,
+       
       });
 
       // Listen for location updates from server
-      socket.on('locationUpdate', (data) {
-        print('Received location update: $data');
-        // Handle location update received from server
-        // Update UI, etc.
+      socket.on('locationChange', (data) {
+        // Print received data
+        print("Received location change data from server: $data");
       });
+      socket.onConnectError((error) {
+        print("Connection error occurred: $error");
+      });*/
     } catch (e) {
       print('Error getting location: $e');
-      // Handle error here, e.g., show an error message to the user
     }
   }
 
@@ -83,6 +120,108 @@ class _LocationTrackerPageState extends State<LocationTrackerPage> {
         ),
         myLocationEnabled: true, // Show the user's location
         myLocationButtonEnabled: true, // Enable the my location button
+      ),
+    );
+  }
+}*/
+
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:socket_io_client/socket_io_client.dart';
+import 'web_socket.dart';
+
+class LocationTrackerPage extends StatefulWidget {
+  const LocationTrackerPage({super.key});
+
+  @override
+  createState() => _LocationTrackerPageState();
+}
+
+class _LocationTrackerPageState extends State<LocationTrackerPage> {
+  late Position _currentPosition;
+  late GoogleMapController _mapController;
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+    _requestLocationPermission();
+  }
+
+  // Initialize the GoogleMapController in the onMapCreated callback
+  void _onMapCreated(GoogleMapController controller) {
+    _mapController = controller;
+  }
+
+  // Request location permission
+  void _requestLocationPermission() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      
+      print('Location permission denied');
+    } else if (permission == LocationPermission.deniedForever) {
+      
+      print('Location permission denied forever');
+    } else {
+      
+      getCurrentLocation();
+    }
+  }
+
+  
+  void getCurrentLocation() async {
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+      setState(() {
+        _currentPosition = position;
+      });
+
+      print('_mapController initialized? ${_mapController != null}');
+      // Now that the controller is initialized, move the camera to current location
+      _mapController.animateCamera(
+        CameraUpdate.newLatLng(
+          LatLng(_currentPosition.latitude, _currentPosition.longitude),
+        ),
+      );
+     
+
+      if (socket.connected) {
+        try {
+          socket.emit('updateLocation', [_currentPosition.latitude, _currentPosition.longitude]);
+        
+        } catch (e) {
+          print('Error emitting updateLocation event: $e');
+        }
+
+        socket.on('locationChange', (data) {
+          print('Location update: $data');
+        });
+       
+      } else {
+        print('Socket is not connected');
+      }
+    } catch (e) {
+      print('Error getting location: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Location Tracker'),
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated, // Initialize controller in onMapCreated
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(0, 0), 
+          zoom: 12, 
+        ),
+        myLocationEnabled: true, 
+        myLocationButtonEnabled: true, 
       ),
     );
   }
