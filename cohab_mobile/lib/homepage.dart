65de7531144bd9635cd72e8d8f5d2d831be7fd45
Ethan Fuller <etjamir14@gmail.com>
2024-04-12 11:26:05 -0400

    no dashboard

--- cohab_mobile/lib/homepage.dart
+++ cohab_mobile/lib/homepage.dart
@@ -2,7 +2,6 @@ import 'package:cohab_mobile/calendar.dart';
 import 'package:cohab_mobile/groupchat.dart';
 import 'package:cohab_mobile/location.dart';
 import 'package:cohab_mobile/noise_level.dart';
-import 'package:cohab_mobile/dashboard.dart';
 import 'package:cohab_mobile/settings.dart';
 import 'package:cohab_mobile/task_list.dart';
 import 'package:flutter/material.dart';
@@ -38,7 +37,7 @@ class _HomePageState extends State<HomePage> {
                       onTap: () {
                         Navigator.push(
                           context,
-                          MaterialPageRoute(builder: (context) => const DashboardPage()),
+                          MaterialPageRoute(builder: (context) => const NoiseLevelPage()),
                         );
                       },
                       child: Ink(
@@ -54,18 +53,18 @@ class _HomePageState extends State<HomePage> {
                               onTap: () {
                                 Navigator.push(
                                   context,
-                                  MaterialPageRoute(builder: (context) => DashboardPage()),
+                                  MaterialPageRoute(builder: (context) => const NoiseLevelPage()),
                                 );
                               },
                               child: const Icon(
-                                LucideIcons.history,
+                                LucideIcons.music,
                                 color: Colors.white,
                                 size: 48,
                               ),
                             ),
                             const SizedBox(height: 8),
                             const Text(
-                              'Recent Activity',
+                              'Noise Level',
                               style: TextStyle(color: Colors.white, fontSize: 17),
                             ),
                           ],
@@ -296,47 +295,6 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
               const SizedBox(height: 80),
-              SizedBox(
-                width: 120,
-                child: InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const NoiseLevelPage()),
-                    );
-                  },
-                  child: Ink(
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF14532d), // Fill the button with green color
-                      borderRadius: BorderRadius.circular(8.0),
-                      border: Border.all(color: const Color(0xFF14532d)), // Green border
-                    ),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        InkWell(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => const NoiseLevelPage()),
-                            );
-                          },
-                          child: const Icon(
-                            LucideIcons.music,
-                            color: Colors.white,
-                            size: 48,
-                          ),
-                        ),
-                        const SizedBox(height: 8),
-                        const Text(
-                          'Noise Level',
-                          style: TextStyle(color: Colors.white, fontSize: 17),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
             ],
           ),
         ),
