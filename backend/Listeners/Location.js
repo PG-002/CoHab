06cd3b4f4ddc70b8c048da246ac59b118ac2d0a7
RyanGarfinkel <113050972+RyanGarfinkel@users.noreply.github.com>
2024-04-16 13:19:32 -0400

    LocationChange return update

--- backend/Listeners/Location.js
+++ backend/Listeners/Location.js
@@ -1,16 +1,28 @@
 const User = require('../Models/User');
+const { use } = require('../Routes/UserRoutes');
 
 module.exports = (socket, io) => {
 
     const updateObj = (lat, long) => ({
-        'location.latitude' : lat, 
-        'location.longitude' : long, 
+        'location.latitude' : parseInt(lat + ''), 
+        'location.longitude' : parseInt(long + ''), 
         'location.lastUpdated' : Date.now()
     })
 
+    const getRoomateLocations = async () => {
+
+        const users = await User.find({ houseId : socket.room, 'location.isTracking' : true })
+            .catch(() => null);
+
+        if(!users)
+            return [];
+
+        return users.map(user => ({ name : user.firstName + ' ' + user.lastName, lat : user.location.latitude, long : user.location.longitude, lastUpdated : user.location.lastUpdated }))
+    }
+
     socket.on('updateLocation', async (lat, long) =>
         await User.findByIdAndUpdate(socket.user._id, { $set : updateObj(lat, long) }, { new : true })
-            .then(user => io.to(socket.room).emit('locationChange', { userId : user._id, location : user.location }))
+            .then(async () => io.to(socket.room).emit('locationChange', await getRoomateLocations()))
             .catch(err => console.log(err))
     );
 }
