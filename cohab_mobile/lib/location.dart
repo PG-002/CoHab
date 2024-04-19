a6ed0888e037b1664b354231bdf93189cb66ef52
ellieteter <156714215+ellieteter@users.noreply.github.com>
2024-04-19 18:25:15 -0400

    location should be working! ..i hope

--- cohab_mobile/lib/location.dart
+++ cohab_mobile/lib/location.dart
@@ -1,8 +1,11 @@
 
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:socket_io_client/socket_io_client.dart';
+import 'package:http/http.dart'
+    as http; // Import the http package for making network requests
+import 'token.dart';
 import 'web_socket.dart';
 
 class LocationTrackerPage extends StatefulWidget {
@@ -14,18 +17,26 @@ class LocationTrackerPage extends StatefulWidget {
 
 class _LocationTrackerPageState extends State<LocationTrackerPage> {
   late Position _currentPosition;
-  late GoogleMapController _mapController;
+  GoogleMapController? _mapController;
+
+  List<Map<String, dynamic>> _userLocations = [];
+  Set<Marker> _markers = {};
 
   @override
-  void initState()  {
+  void initState() {
     super.initState();
-    init();
     _requestLocationPermission();
+    //socket.on('locationChange', _onLocationChange);
+    socket.on('locationChange', (data) {
+      print('Location update: $data');
+      _onLocationChange(data);
+    });
   }
 
-  // Initialize the GoogleMapController in the onMapCreated callback
   void _onMapCreated(GoogleMapController controller) {
     _mapController = controller;
+
+    _setMarkers();
   }
 
   // Request location permission
@@ -33,8 +44,6 @@ class _LocationTrackerPageState extends State<LocationTrackerPage> {
     LocationPermission permission = await Geolocator.requestPermission();
     if (permission == LocationPermission.denied) {
       print('Location permission denied');
-    } else if (permission == LocationPermission.deniedForever) {
-      print('Location permission denied forever');
     } else {
       getCurrentLocation();
     }
@@ -45,29 +54,44 @@ class _LocationTrackerPageState extends State<LocationTrackerPage> {
       Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high,
       );
-      setState(() {
-        _currentPosition = position;
-      });
 
+      if (mounted) {
+        setState(() {
+          _currentPosition = position;
+        });
+      }
+
+      if (_mapController == null) {
+        print('Map controller is not initialized yet.');
+        return;
+      }
       print('_mapController initialized? ${_mapController != null}');
-      // Now that the controller is initialized, move the camera to current location
-      _mapController.animateCamera(
-        CameraUpdate.newLatLng(
-          LatLng(_currentPosition.latitude, _currentPosition.longitude),
-        ),
-      );
+
+      if (_mapController != null) {
+        _mapController!.animateCamera(
+          CameraUpdate.newLatLng(
+            LatLng(_currentPosition.latitude, _currentPosition.longitude),
+          ),
+        );
+      }
 
       if (socket.connected) {
         try {
+          print([_currentPosition.latitude, _currentPosition.longitude]);
           socket.emit('updateLocation',
               [_currentPosition.latitude, _currentPosition.longitude]);
         } catch (e) {
           print('Error emitting updateLocation event: $e');
         }
-
         socket.on('locationChange', (data) {
           print('Location update: $data');
+          _onLocationChange(data);
         });
+
+        /*socket.on('locationChange', (data) {
+          //print("Hey");
+          print('Location update: $data');
+        });*/
       } else {
         print('Socket is not connected');
       }
@@ -76,23 +100,144 @@ class _LocationTrackerPageState extends State<LocationTrackerPage> {
     }
   }
 
-  @override
+  void _onLocationChange(dynamic data) {
+    if (mounted) {
+      setState(() {
+        _userLocations = List<Map<String, dynamic>>.from(data);
+      });
+    }
+    print("Printing user locations: ");
+    print(_userLocations);
+    _setMarkers();
+  }
+
+  void _setMarkers() async {
+    Set<Marker> markers = {};
+    for (var userLocation in _userLocations) {
+      String name = userLocation['name'] ?? 'Unknown User';
+      double latitude = userLocation['lat'] ?? 0.0;
+      double longitude = userLocation['long'] ?? 0.0;
+
+      LatLng position = LatLng(latitude, longitude);
+      String markerId = '${name}_$latitude$longitude';
+
+      String iconUrl =
+          'https://ui-avatars.com/api/?name=${Uri.encodeComponent(name)}&background=bbf7d0&color=052e16&bold=true&size=128';
+
+      BitmapDescriptor icon = await _loadCustomIcon(iconUrl);
+
+      Marker marker = Marker(
+        markerId: MarkerId(markerId),
+        position: position,
+        icon: icon,
+        infoWindow: InfoWindow(
+          title: name,
+          snippet: 'Location of $name',
+        ),
+      );
+
+      markers.add(marker);
+    }
+
+    if (mounted) {
+      setState(() {
+        _markers = markers;
+      });
+    }
+  }
+
+  Future<BitmapDescriptor> _loadCustomIcon(String url) async {
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        Uint8List imageData = response.bodyBytes;
+        return BitmapDescriptor.fromBytes(imageData);
+      } else {
+        print('Failed to load icon from URL: $url');
+        return BitmapDescriptor.defaultMarker;
+      }
+    } catch (e) {
+      print('Error loading icon: $e');
+      return BitmapDescriptor.defaultMarker;
+    }
+  }
+
+  void zoomIn() {
+    if (_mapController != null) {
+      _mapController!.animateCamera(CameraUpdate.zoomIn());
+    }
+  }
+
+  void zoomOut() {
+    if (_mapController != null) {
+      _mapController!.animateCamera(CameraUpdate.zoomOut());
+    }
+  }
+
+  /*@override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('          Location Tracker'),
+        title: const Text('Location Tracker'),
       ),
       body: GoogleMap(
-        onMapCreated: _onMapCreated, // Initialize controller in onMapCreated
+        onMapCreated: _onMapCreated,
         initialCameraPosition: const CameraPosition(
           target: LatLng(0, 0),
           zoom: 12,
         ),
         myLocationEnabled: true,
         myLocationButtonEnabled: true,
+        markers: _markers, 
+      ),
+      
+      
+    );
+  }*/
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Location Tracker'),
+      ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: const CameraPosition(
+              target: LatLng(0, 0),
+              zoom: 12,
+            ),
+            myLocationEnabled: true,
+            myLocationButtonEnabled: true,
+            markers: _markers,
+          ),
+          // Positioning the zoom buttons
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: Column(
+              children: [
+                // Zoom In button
+                FloatingActionButton(
+                  heroTag: 'zoom_in_fab',
+                  onPressed: zoomIn,
+                  tooltip: 'Zoom In',
+                  child: const Icon(Icons.add),
+                ),
+                const SizedBox(height: 10),
+                // Zoom Out button
+                FloatingActionButton(
+                  heroTag: 'zoom_out_fab',
+                  onPressed: zoomOut,
+                  tooltip: 'Zoom Out',
+                  child: const Icon(Icons.remove),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
-
 }
-
